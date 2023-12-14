03bbdc563ac79fb382e7773d6b32fcd1276484ed
SnapITjesse <jesse.juarez@snapit.solutions>
2023-12-14 11:55:30 -0600

    updated text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Updated text for my lab!'));
+app.get('/', (req, res) => res.send('Updated text for my lab! This is where i changeed the text for new results in the pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
